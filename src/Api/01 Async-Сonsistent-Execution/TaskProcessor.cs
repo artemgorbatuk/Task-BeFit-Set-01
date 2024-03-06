c2ef1c5a5ae319f067ccc33e32739018e1c8dffd
Artem Gorbatuk <gorbatuk.am@gmail.com>
2024-03-06 14:44:41 +0300

    TaskProcessor создан

---
+++ src/Api/01 Async-Сonsistent-Execution/TaskProcessor.cs
@@ -0,0 +1,29 @@
+﻿namespace _01_Async_Сonsistent_Execution
+{
+    public class TaskProcessor
+    {
+        private Queue<Task> tasks = new();
+
+        public async Task AddTask(Task task)
+        {
+            tasks.Enqueue(task);
+
+            if (tasks.Count == 1)
+                await ProcessTasks();
+        }
+
+        private async Task ProcessTasks()
+        {
+            while (tasks.Count > 0)
+            {
+                await Task.Run(tasks.Peek);
+                _ = tasks.Dequeue();
+            }
+        }
+
+        public async Task WaitForCompletion()
+        {
+            await Task.WhenAll(tasks);
+        }
+    }
+}
